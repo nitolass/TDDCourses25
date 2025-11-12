2a8da4c8adb694afacc91095097bee112567df41
PORTATILHP\nito9 <tamboleojuan2005@gmail.com>
2025-11-12 15:56:36 +0100

    Paso 7

---
+++ tests/Feature/Models/CourseTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Models\Course;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+test('only returns released courses for released scope', function () {
+    //Arrange
+    Course::factory()->released()->create();
+    Course::factory()->create();
+
+    //Act & Assert
+
+    expect(Course::released()->get())
+        ->toHaveCount(1)
+        ->first()->id->toEqual(1);
+
+});
