ac0715a037b7d6111dde1dfad35b243ce33f9abc
PORTATILHP\nito9 <tamboleojuan2005@gmail.com>
2025-11-21 18:41:31 +0100

    Paso 13

---
+++ database/migrations/2025_11_07_195953_create_courses_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug');
+            $table->string('tagline');
+            $table->string('title');
+            $table->string('description');
+            $table->string('image_name');
+            $table->json('learnings');
+            $table->timestamp('released_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
